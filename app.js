1de8720aa148c15b4ed850cc1f5f06b177eb73fa
chanjal28 <59991172+chanjal28@users.noreply.github.com>
2023-10-17 16:14:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
+
